e29e65b5df3415b2638da10535181707a486f517
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-08-22 10:58:06 +1200

    comments
    i added comments so the code is more understandable to others as it shows what the code is doing

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// list of players with their scores
 userArray = [
   { name: 'Max', score: 6 },
   { name: 'Bella', score: 2 },
   { name: 'Cooper', score: 3 },
 ]
+//variables used within both functions
 let userName
 let userGuess
 let guesses = 0
@@ -11,23 +13,23 @@ const min = 1
 const max = 20
 
 function playGame() {
+  //asks for name, makes a random number from 1-20 and then continues to ask the user for a number until they guess the numver right
   let guesses = 0
+
   //get user name
   userName = prompt('What is your name?')
 
   //get random number
   let computerNumber = Math.floor(Math.random() * (max - min + min) + min)
 
-  alert(computerNumber)
-
-  // compare guess with number while loop
+  //  runs code until the user guesses the number correct
   while (computerNumber != userGuess) {
     //gets guess from user
     userGuess = prompt('What number do you guess?')
 
     //verifies guess
     if (userGuess.match(/^([1-9]|1[0-9]|20)$/)) {
-      // see how many times it has taken
+      // see how many guesses have been said
       guesses = guesses + 1
     } else {
       alert('input not valid')
@@ -37,17 +39,19 @@ function playGame() {
   //show number of guesses
   alert('you got the number correct after ' + guesses + ' guesses')
 
-  //makes object
+  //combines user information
   user = {
     //lets user input in name
     name: userName,
     //score of game
     score: guesses,
   }
+  //adds user information to score board
   userArray.push(user)
 }
 
 function scoreBoard() {
+  //shows name and score of every user in the array
   for (let user of userArray) {
     alert(user.name + ', ' + user.score)
   }
