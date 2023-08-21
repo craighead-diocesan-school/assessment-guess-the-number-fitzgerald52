e74cf6033ae00957d0242ada0263c40877b4dc41
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-08-21 12:19:56 +1200

    expected inputs
    i tested the playGame( ) function by inputing numbers of 2 5 3 12 and then 18 (the correct number) , and got the expected output of what number do you want to choses 4 times and then the expected out put of that is the correct number you guessed it after 4 guesses

---
+++ script.js
@@ -0,0 +1,47 @@
+userArray = [
+  { name: 'Max', score: 6 },
+  { name: 'Bella', score: 2 },
+  { name: 'Cooper', score: 3 },
+]
+let userName
+let userGuess
+let guesses = 0
+let user
+
+function playGame() {
+  let guesses = 0
+  //get user name
+  userName = prompt('What is your name?')
+
+  //get random number
+  let computerNumber = Math.floor(Math.random() * 20)
+
+  alert(computerNumber)
+
+  // compare guess with number while loop
+  while (computerNumber != userGuess) {
+    //get guess
+    userGuess = prompt('What number do you guess?')
+
+    // see how many times it has taken
+    guesses = guesses + 1
+  }
+
+  //show number of guesses
+  alert('you got the number correct after ' + guesses + ' guesses')
+
+  //makes object
+  user = {
+    //lets user input in name
+    name: userName,
+    //score of game
+    score: guesses,
+  }
+  userArray.push(user)
+}
+
+function scoreBoard() {
+  for (let user of userArray) {
+    alert(user.name + ', ' + user.score)
+  }
+}
